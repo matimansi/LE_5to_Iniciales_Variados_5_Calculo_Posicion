d546b62836fed09e53d669c303f894afd831fe4d
Matias Mansilla <mmansilla@etrr.edu.ar>
2022-11-10 20:29:33 -0300

    1.0V

--- Calculo_Posicion.c
+++ Calculo_Posicion.c
@@ -1,11 +1,6 @@
 
 /*
-	Calculo la posición de un automovil que se desplaza con un MRU por un plano horizontal.
-	Se asume que la posición inicial es 0
-
 	Ecuacion MRU --> P(f) = P(i) + V . T
-
-	>>> TERMINADO <<<
 */
 
 #include <stdio.h>
@@ -16,15 +11,15 @@ float velocidad, tiempo;
 int main()
 {
 	float resultado;
-	printf("Bienvenido a mi programa...\n");
+	printf("\nBienvenido a mi programa...\n");
 	printf("Gracias a este programa podras saber la posicion final de un auto\n");
 	printf("ingresando el tiempo y la velocidad, asumiendo que la posicion inicial es 0\n\n");
 	printf("Ingrese la velocidad en m/s\n>>> ");
 	scanf("%f", &velocidad);
-	printf("Ingrese el tiempo en sgundos\n>>> ");
+	printf("Ingrese el tiempo en segundos\n>>> ");
 	scanf("%f", &tiempo);
 	resultado = calculo_posicion(velocidad, tiempo);
-	printf("La posicion final del auto es: %.3f metros", resultado);
+	printf("La posicion final del auto es: %.3f metros\n\n", resultado);
 	return 0;
 }
 
